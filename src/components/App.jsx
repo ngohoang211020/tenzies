efb8b4500dcbf3431382606bc8eb8d517a351b7c
hoang.ngo <hoanggg2110@gmail.com>
2025-03-23 14:30:22 +0700

    feat: write logic to win game

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,17 @@
 import Die from "./Die"
 import { useState } from "react"
 import { nanoid } from "nanoid/non-secure"
+import Confetti from "react-confetti"
+import { useWindowSize } from 'react-use'
 
 export default function App() {
-  const [dice, setDice] = useState(generateAllNewDice())
+  // Create an array of 10 dice objects with random values and isHeld set to false by default but when App() renders for the first time
+
+  const [dice, setDice] = useState(() => generateAllNewDice())
+
+  // Check if all dice are held and have the same value every time the dice state changes
+  const gameWon = dice.every(dieObj => dieObj.isHeld) && dice.every(dieObj => dieObj.value === dice[0].value)
+   
 
   function generateAllNewDice() {
     return new Array(10)
@@ -16,13 +24,16 @@ export default function App() {
   }
 
   function rollDice() {
+    if(gameWon){
+      setDice(generateAllNewDice())
+      return
+    }
     setDice(dice.map(dieObj => {
       return dieObj.isHeld ? dieObj : {
         ...dieObj,
         value: Math.floor(Math.random() * 6) + 1
       }
-    }
-    ))
+    }))
   }
 
   function heldDie(id) {
@@ -40,15 +51,20 @@ export default function App() {
     .map(dieObj => <Die key={dieObj.id} dieObj={dieObj} heldDie={heldDie}
     />
     )
+  
+  const { width, height } = useWindowSize()
 
   return (
     <main>
+      {gameWon && <Confetti width={width} height={height}/>}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-      <div className="dice-container">
+      <div className="dice-container">  
         {diceElements}
       </div>
-      <button onClick={rollDice} className="roll-dice">Roll</button>
+      <button onClick={rollDice} className="roll-dice">
+        {gameWon ? "New Game" : "Roll"}
+        </button>
     </main>
   )
 
