2cd1579a01c29dd569961d2b89a9465ce6c0195a
hoang.ngo <hoanggg2110@gmail.com>
2025-03-23 19:52:59 +0700

    deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import Die from "./Die"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { nanoid } from "nanoid/non-secure"
 import Confetti from "react-confetti"
 import { useWindowSize } from 'react-use'
@@ -11,8 +11,15 @@ export default function App() {
 
   // Check if all dice are held and have the same value every time the dice state changes
   const gameWon = dice.every(dieObj => dieObj.isHeld) && dice.every(dieObj => dieObj.value === dice[0].value)
-   
 
+  const buttonRef = useRef(null)
+
+  useEffect(() => {
+    if (gameWon) {
+      buttonRef.current.focus()
+    }
+  }, [gameWon])
+  
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
@@ -24,7 +31,7 @@ export default function App() {
   }
 
   function rollDice() {
-    if(gameWon){
+    if (gameWon) {
       setDice(generateAllNewDice())
       return
     }
@@ -51,20 +58,23 @@ export default function App() {
     .map(dieObj => <Die key={dieObj.id} dieObj={dieObj} heldDie={heldDie}
     />
     )
-  
+
   const { width, height } = useWindowSize()
 
   return (
     <main>
-      {gameWon && <Confetti width={width} height={height}/>}
+      {gameWon && <Confetti />}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+      </div>
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-      <div className="dice-container">  
+      <div className="dice-container">
         {diceElements}
       </div>
-      <button onClick={rollDice} className="roll-dice">
+      <button onClick={rollDice} className="roll-dice" ref={buttonRef}>
         {gameWon ? "New Game" : "Roll"}
-        </button>
+      </button>
     </main>
   )
 
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,7 +3,10 @@ export default function Die(props) {
     <button 
     onClick={() => props.heldDie(props.dieObj.id)}
       style={{ backgroundColor: props.dieObj.isHeld ? 'hsla(144, 71%, 62%, 1)' : 'white' }}
-    >
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, 
+      ${props.dieObj.isHeld ? "held" : "not held"}`}
+   >
       {props.dieObj.value}
     </button>
   )
